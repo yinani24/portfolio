f85e64a7e9401d960fdaf760a8212b60a23fc141
Yash <yinani@ucdavis.edu>
2024-01-08 22:15:48 -0800

    new changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,27 @@
 import Link from 'next/link'
 import { navname } from './constants'
+import Image from 'next/image'
+import Logo from '../../public/Logo.png'
 
 export default function NavBar() {
     return (
-      <ul className='fixed flex flex-wrap flex-row justify-around pl-80'>
-        {navname.map((name, index) => (
-          <li key={index}>
-            <button className='border rounded'>
-              <Link href={`${name.route}`}> {name.title}
-              </Link>
-            </button>
-          </li>
-        ))}
-      </ul>
+      <section className='flex flex-wrap flex-row justify-around
+      items-center w-screen p-2 bg-purple-500'>
+        <Link href='/'>
+          <Image className='rounded-lg' src={Logo} width={60} height={60} 
+            alt={"Yash Inani Logo"}>
+          </Image>
+        </Link>
+        <ul className='flex flex-row justify-around items-center w-[30rem]'>
+          {navname.map((name, index) => (
+            <li key={index}>
+              <button className='border rounded w-100 h-100'>
+                <Link href={`${name.route}`}> {name.title}
+                </Link>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
     )
   }
