e71e81d53e78b658f0fc002550d2046aae20c97d
Yash <yinani@ucdavis.edu>
2024-01-13 03:06:57 -0800

    home changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,29 +1,46 @@
+"use client";
+
 import Link from 'next/link'
 import { navname } from './constants'
 import Image from 'next/image'
 import Logo from '../../public/Logo.png'
+import { motion } from 'framer-motion'
 
 export default function NavBar() {
     return (
       <div className='flex w-screen justify-center'>
-      <section className='fixed flex flex-wrap flex-row justify-around
-      items-center w-10/12 p-2 bg-[#7B4AE208] rounded-lg my-5'>
-        <Link href='/'>
-          <Image className='rounded-lg' src={Logo} width={60} height={60} 
-            alt={"Yash Inani Logo"}>
-          </Image>
-        </Link>
-        <ul className='flex flex-row justify-around items-center w-[30rem]'>
-          {navname.map((name, index) => (
-            <li key={index}>
-              <button className='text-[#ffffff] w-100 h-100 hover:text-[#7B4AE280]'>
-                <Link className='font-[raleway] text-center bg-opactiy-50 font-normal text-base hover:font-bold' href={`${name.route}`}> {name.title}
-                </Link>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </section>
+        <motion.section 
+          initial={{ opacity: 0, scale:0.5, y: -100}}
+          animate={{ opacity: 1, scale:1, y: 0}}
+          className='fixed flex flex-wrap flex-row justify-between
+        items-center w-10/12 p-2 bg-[#7B4AE208] rounded-lg my-5'
+        >
+          <motion.button 
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className='mx-32'>
+            <Link href='/'>
+              <Image className='rounded-lg' src={Logo} width={60} height={60} 
+                alt={"Yash Inani Logo"}>
+              </Image>
+            </Link>
+          </motion.button>
+          <ul className='flex flex-row justify-around items-center w-[30rem]'>
+            {navname.map((name, index) => (
+              <li key={index}>
+                <motion.button 
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  className='text-[#ffffff] w-100 h-100 hover:text-[#7B4AE280]'>
+                    <Link className='font-[raleway] text-center bg-opactiy-50 
+                      font-normal text-base hover:font-bold' href={`${name.route}`}> 
+                        {name.title}
+                    </Link>
+                </motion.button>
+              </li>
+            ))}
+          </ul>
+        </motion.section>
       </div>
     )
   }
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,20 +1,41 @@
+"use client";
+
 import { TypeAnimation } from "react-type-animation"
 import {names} from '../components/constants'
+import Image from 'next/image'
+import Github from '../../public/Github.png'
+import LinkedIn from '../../public/LinkedIn.png' 
+import Link from "next/link";
 
 export default function Home() {
     return (
       <section id='home'>
         <div>
           <div className='rounded-lg bg-[#7B4AE208]'>
-            <p className='text-purple-600 font-[Raleway] font-bold text-lg text-center'>👋 Hello!!</p>
+            <p className='text-white font-[Raleway] font-bold text-lg text-center'>👋 Hello!!</p>
           </div>
-          {/* <TypeAnimation
-            sequence={names.map((name) => [}
-          >
-          </TypeAnimation> */}
+          <h1 className='text-white font-[Raleway] font-bold text-9xl text-center'>
+            <span className='text-[#7B4AE2]'>I'm
+            <TypeAnimation
+            sequence={names.flatMap(({name, time}) => [name, time])}
+            wrapper="h1"
+            speed={50}
+            repeat={Infinity}
+            />
+            </span>
+            
+          </h1>
+          
+          <div>
+          <Link href='https://www.github.com/yinani24/'>
+            <Image alt='Github' className='rounded-lg' src={Github} width={60} height={60}/>
+          </Link>
+          <Link href='https://www.linkedin.com/in/yash-inani/'>
+            <Image alt='LinkedIn' className='rounded-lg' src={LinkedIn} width={60} height={60}/>
+          </Link>
+          </div>
+          
         </div>
-        
-
       </section>
     )
   }
