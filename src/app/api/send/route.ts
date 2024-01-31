5f4ed9f8f2e0f3578387279772b2e3e1561cb9a9
Yash <yinani@ucdavis.edu>
2024-01-30 23:58:23 -0800

    changes in contact page

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,19 +1,28 @@
+"use server";
+
+import { NextApiRequest, NextApiResponse } from 'next';
 import { EmailTemplate } from '@/components/emailtemplate';
 import { Resend } from 'resend';
+import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL || '';
+const toEmail = process.env.TO_EMAIL || '';
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ email: "temp", subject: "temp", body: "temp" }),
-    });
-
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.body.json())
+    const { email, subject, message } = req.body.json();
+    
+    try {
+        const data = await resend.emails.send({
+            from: fromEmail,
+            to: toEmail,
+            subject: "Contact Form Submission",
+            reply_to: email,
+            react: EmailTemplate({ email: email, subject: subject, body: message }),
+        });
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({error});
+    }
 }
--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -1,20 +1,59 @@
+"use client";
+
+import React from "react";
 import { FiSend } from "react-icons/fi";
+import { toast } from "react-hot-toast";
 
 export default function ContactForm(){
+    
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const data = {
+          email: e.target.senderEmail.value,
+          subject: e.target.subject.value,
+          message: e.target.message.value,
+        };
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "/api/send";
+    
+        // Form the request for sending data to the server.
+        const options = {
+          // The method is POST because we are sending data.
+          method: "POST",
+          // Tell the server we're sending JSON.
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Body of the request is the JSON data we created above.
+          body: JSONdata,
+        };
+    
+        const response = await fetch(endpoint, options);
+        console.log(response);
+        
+        if (response.status === 200) {
+            console.log("Message sent.");
+            toast.success("Message sent successfully!");
+            e.target.reset()
+        }else {
+            console.log("Message failed to send.");
+            toast.error("Message failed to send.");
+        }
+    };
+
     return(
         <div className='flex flex-col items-center justify-center mt-10'>
-            <form className='flex flex-col items-start justify-center border border-[#7B4AE280] rounded-2xl p-5'>
+            <form onSubmit={handleSubmit} className='flex flex-col items-start justify-center border border-[#7B4AE280] rounded-2xl p-5'>
                 <label className='text-[#7B4AE2] font-Raleway text-lg font-bold mb-4 mt-2'>Email</label>
-                <input className='w-96 h-12 rounded-xl p-4 mb-4 text-[rgba(255, 255, 255, 0.5)] font-Raleway' type='email' placeholder='alan.turing@example.com'/>
+                <input name="senderEmail" className='w-96 h-12 rounded-xl p-4 mb-4 text-[rgba(255, 255, 255, 0.5)] font-Raleway' type='email' placeholder='alan.turing@example.com'/>
                 <label className='text-[#7B4AE2] font-Raleway font-bold text-lg mb-4'>Subject</label>
-                <input className='w-96 h-12 rounded-xl p-4 mb-4 text-[rgba(255, 255, 255, 0.5)] font-Raleway' type='text' placeholder={`Let's build a company together`}/>
+                <input name="subject" className='w-96 h-12 rounded-xl p-4 mb-4 text-[rgba(255, 255, 255, 0.5)] font-Raleway' type='text' placeholder={`Let's build a company together`}/>
                 <label className='text-[#7B4AE2] font-Raleway font-bold text-lg mb-4'>Message</label>
-                <textarea className='w-96 h-36 rounded-xl p-4 mb-4 text-[rgba(255, 255, 255, 0.5)] font-Raleway' 
+                <textarea name='message' className='w-96 h-36 rounded-xl p-4 mb-4 text-[rgba(255, 255, 255, 0.5)] font-Raleway' 
                     placeholder={`Hey Yash,\nI love your website...`}/>
-                <div className="flex w-96 h-12 items-center justify-center text-white">
-                    <button className='w-96 h-12 rounded-xl mb-4 bg-[#7B4AE2] text-white text-Raleway font-bold text-center'>Send</button>
-                    <FiSend className='w-full relative ml-8'/>
-                </div>
+                <button type="submit" className='w-96 group text-base flex justify-center outline-none items-center gap-2 h-12 rounded-xl bg-[#7B4AE2] mb-4 text-white text-Raleway font-bold text-center transition-all focus:scale-110 '>
+                    Send{" "}<FiSend className='text-xs'/>
+                </button>
             </form>
         </div>
     )
